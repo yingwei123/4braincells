1631277e2535220b885c99c918f4f3fc28b960a7
nafin147 <43007741+nafin147@users.noreply.github.com>
2021-04-17 21:16:16 -0400

    Frontend layout

---
+++ views/Home/home.js
@@ -0,0 +1,13 @@
+let type = 'message'
+function getMessages(id) {
+    console.log(id)
+}
+function changePeople(){
+    document.getElementById('People').style.color = '#000000';
+    document.getElementById('Message').style.color = '#bababa';
+
+}
+function changeMessage(){
+    document.getElementById('People').style.color = '#bababa';
+    document.getElementById('Message').style.color = '#000000';
+}
