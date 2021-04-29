cd1e1d4a2c4c26014dd2fad545bcb5525c261167
wpdoughe <47640356+wpdoughe@users.noreply.github.com>
2021-04-28 21:50:50 -0400

    Profile Picture Functionality
    Completed profile picture functionality. Users can now change their profile picture at any point and those changes are stored locally and saved to the Mongodb database.

---
+++ 4braincells-main/routes/chatRoutes.js
@@ -0,0 +1,68 @@
+const Users = require('../models/Users')
+const chatFunc = require('./chatFunc')
+const e = require('express');
+
+module.exports = app =>{
+
+    //get all chatrooms
+    app.get("/allChatRoom", async(req,res) =>{
+        let allChat = await chatFunc.getAllChatRoom()
+            res.send(allChat)
+    })
+
+    app.get("/deleteAllChatroom",async(req,res) =>{
+        let deleted = await chatFunc.deleteChat()
+        res.send(deleted)
+        })
+
+        // create a chat room if one doesnt exist and send message, if exist send message between
+        app.post("/getChatRoom", async(req,res)=>{
+
+          try{
+            personA = req.body.personA
+            personB = req.body.personB
+
+
+
+            let chatRoom = await chatFunc.findChatRoom(personA, personB)
+            res.send(chatRoom)
+          }catch(err){
+              res.send(err)
+          }
+
+    })
+    app.post("/newChat", async(req,res)=>{
+        try{
+            console.log(req.body)
+            const sender = await Users.findOne({_id: req.body.sender});
+            const receiver = await Users.findOne({_id: req.body.receiver});
+            const room = await chatFunc.createChatRoom(sender, receiver)
+            console.log({receiver: receiver, chatRoom: room})
+            res.send({receiver: receiver, chatRoom: room});
+        }catch(err){
+            console.log(err)
+            res.send(err)
+        }
+    })
+
+    app.post("/getMessages", async(req,res)=>{
+        try{
+            console.log("trigger")
+            let chatRoom = await chatFunc.getMessages(req.body.chatroom_id)
+            console.log(chatRoom)
+            res.send(chatRoom)
+        }catch(err){
+            res.send(err)
+        }
+    })
+
+    app.post("/findChatroom", async(req,res)=>{
+        try{
+            let x = await chatFunc.findChatRoom(req.body.token, req.body.reciever)
+            console.log(x)
+            res.send(x)
+        }catch(err){
+            res.send(err)
+        }
+    })
+}
---
+++ 4braincells-main/views/Login/Signin.js
@@ -0,0 +1,96 @@
+let login = true;
+let showPass = false;
+async function sendRequest(){
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
+    const name = document.getElementById("name").value.trim();
+    if(checkEmail(email) && checkPass(password)){
+        if (name) {
+            const request = await fetch('/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({email:email, password:password, name:name}),
+            });
+            if (!request.ok){
+                document.getElementById("InvalidLogin").style.display = "block";
+            }
+            else if (request.redirected) {
+                window.location.href = request.url;
+            }
+        }else {
+            const request = await fetch('/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({email:email, password:password})
+            });
+            if (!request.ok){
+                document.getElementById("InvalidLogin").style.display = "block";
+            }
+            else if (request.redirected) {
+                window.location.href = request.url;
+            }
+        }
+    }
+}
+function passwordVisibility(){
+    if (showPass){
+        document.getElementById("hide").style.display = "block";
+        document.getElementById("show").style.display = "none";
+        document.getElementById("password").type = "password";
+    }
+    else {
+        document.getElementById("show").style.display = "block";
+        document.getElementById("hide").style.display = "none";
+        document.getElementById("password").type = "text";
+        document.getElementById("showPass").style.marginLeft = "23px";
+    }
+    showPass = !showPass;
+}
+function checkEmail(input){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const valid = re.test(String(input).toLowerCase());
+    if(!valid){
+        document.getElementById("email").style.borderColor = "red";
+        document.getElementById("invalidEmail").style.display = "block";
+    }else {
+        document.getElementById("email").style.borderColor = "gray";
+        document.getElementById("invalidEmail").style.display = "none";
+    }
+    return valid;
+}
+
+function checkPass(input){
+    const invalid = input.length < 8;
+    if (invalid){
+        document.getElementById("invalidPass").textContent = "Password less than 8 characters"
+        document.getElementById("invalidPass").style.display = "block";
+        document.getElementById("password").style.borderColor = "red";
+    }else {
+        document.getElementById("invalidPass").style.display = "none";
+        document.getElementById("password").style.borderColor = "gray";
+    }
+    return !invalid;
+}
+function switchLogins(){
+    if(!login){
+        document.getElementById("name").style.display = "none";
+        document.getElementById("login").textContent= "Login";
+        document.getElementById("loginButton").textContent= "Login";
+        document.getElementById("line").style.width = "80px";
+        document.getElementById("switchText").textContent = "Don't have an account?";
+        document.getElementById("switch").textContent = "Create One";
+        document.getElementById("name").value = ""
+    }else {
+        document.getElementById("name").style.display = "block";
+        document.getElementById("login").textContent= "Create Account";
+        document.getElementById("loginButton").textContent= "Create";
+        document.getElementById("line").style.width = "215px";
+        document.getElementById("switchText").textContent = "Have an account?";
+        document.getElementById("switch").textContent = "Login";
+    }
+    login = !login;
+}
